c8c8cbe85d91d61f49baa181b798ed7181ea9886
zengxiang <2914478050@qq.com>
2023-01-05 21:49:35 +0800

    第一次提交

---
+++ src/main/java/com/example/winter_in_changsha/WinterInChangshaApplication.java
@@ -0,0 +1,13 @@
+package com.example.winter_in_changsha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterInChangshaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinterInChangshaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/winter_in_changsha/WinterInChangshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.winter_in_changsha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterInChangshaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
